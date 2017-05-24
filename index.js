25f5a20498e62b9a680e07c9bfbc7a2d53a4dfeb
nicolaxs69 <nicoescobar69@gmail.com>
2017-05-24 15:39:32 -0500

    Server port

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const logger 	   = require('morgan');
 const router 	   = express.Router();
 //const port 	   = process.env.PORT || 8080;
+app.set('port', (process.env.PORT || 5000));
+
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
@@ -13,9 +15,14 @@ app.use(logger('dev'));
 require('./routes')(router);
 app.use('/api/v1', router);
 
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 // app.listen(port);
 //console.log(`App Runs on ${port}`);
 
-app.listen(process.env.PORT || 5000, function () {
-  console.log('Server running');
-})
+//app.listen(process.env.PORT || 5000, function () {
+  //console.log('Server running');
+//})
