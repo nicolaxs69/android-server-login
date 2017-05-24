08ab2c621af001b9a349a2189857ab64535235e3
nicolaxs69 <nicoescobar69@gmail.com>
2017-05-24 15:27:41 -0500

    Server port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app        = express();
 const bodyParser = require('body-parser');
 const logger 	   = require('morgan');
 const router 	   = express.Router();
-const port 	   = process.env.PORT || 8080;
+//const port 	   = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
@@ -13,6 +13,10 @@ app.use(logger('dev'));
 require('./routes')(router);
 app.use('/api/v1', router);
 
-app.listen(port);
+// app.listen(port);
 
-console.log(`App Runs on ${port}`);
+app.listen(process.env.PORT || 8080, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
+});
+
+//console.log(`App Runs on ${port}`);
